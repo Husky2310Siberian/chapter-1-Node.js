3042daf9eb80a5689ace381c76536509ac89b925
Siberian <vasileios.kritikos@aueb.gr>
2024-10-20 20:25:33 +0300

    node changes with data 2

--- products/routes/user.product.routes.js
+++ products/routes/user.product.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const userProductController = require('../controllers/user.product.controller');
 
-router.get('/', userProductController.findAll);
-router.get('/:username', userProductController.findOne);
-router.post('/', userProductController.create)
-router.patch('/:username', userProductController.update)
-router.delete('/:username/products/:id', userProductController.delete)
+router.get('/', userProductController.findUsersProducts);
+router.get('/:username', userProductController.findUserProducts);
+router.post('/:username/products', userProductController.insertUserProduct)
+router.patch('/:username/product/:id', userProductController.update)
+router.delete('/:username/products/:id', userProductController.deleteUserProduct)
 module.exports = router;
