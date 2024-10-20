04773961ed7593ab7d9bd3c4d84fa114680033da
Siberian <vasileios.kritikos@aueb.gr>
2024-10-20 19:29:57 +0300

    node changes

--- products/index.js
+++ products/index.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 const mongoose = require('mongoose');
 
+app.use(express.json()); 
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {console.log('Connction to MongoDB establishe')},
     err => {console.log('Failed to connect to MongoDB')}
@@ -13,7 +15,11 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const user = require('./routes/user.routes');
 
+const userProduct = require('./routes/user.product.routes');
+
 app.use('/api/user', user);
+
+app.use('/api/user.product', userProduct)
 
 app.listen(port, () => {
     console.log("Server is up")
--- products/routes/user.routes.js
+++ products/routes/user.routes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 
+//read methods
 router.get('/', userController.findAll);
 router.get('/:surname', userController.findOne);
+
+//create methods
+router.post('/', userController.create)
+
+//update methods
+router.patch('/:username', userController.update);
+
+//delete methods
+router.delete('/:username', userController.delete);
 
 module.exports = router;
